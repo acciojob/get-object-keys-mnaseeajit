f866dd9272b4cb91f3d0d1e52ff7c0038d33a734
mnaseeajit <147361506+mnaseeajit@users.noreply.github.com>
2024-01-11 16:04:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+let student = {
+	name : "mansi"
+};
+
+Object.prototype.getKeys = function(){
+	return Object.keys(this);
+};
+
+console.log(student.getKeys());
+
